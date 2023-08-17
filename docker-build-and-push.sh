ebd483914b2238eddb3a15a8df711fb6228568e5
kosmik-konan <domagoj@polycode.sh>
2023-08-17 13:42:13 +0200

    change Docker image name

--- docker-build-and-push.sh
+++ docker-build-and-push.sh
@@ -19,8 +19,8 @@ version=$(cat package.json \
   | sed 's/[",]//g')
 
 version="$(echo -e "${version}" | sed -e 's/^[[:space:]]*//')"
-echo "Docker image: ampnet/contracts-manifest-service:$version"
-docker build -t ampnet/contracts-manifest-service:$version -t ampnet/contracts-manifest-service:latest .
+echo "Docker image: polyflowdev/contracts-manifest-service:$version"
+docker build -t polyflowdev/contracts-manifest-service:$version -t polyflowdev/contracts-manifest-service:latest .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push ampnet/contracts-manifest-service:$version
-docker push ampnet/contracts-manifest-service:latest
+docker push polyflowdev/contracts-manifest-service:$version
+docker push polyflowdev/contracts-manifest-service:latest
